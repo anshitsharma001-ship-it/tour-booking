837676fdc63aadb3f168671dd755a5fe4118367f
anshitsharma001-ship-it <anshitsharma001@gmail.com>
2026-03-09 15:34:37 +0530

    update global.css

--- app/components/navs.tsx
+++ app/components/navs.tsx
@@ -11,7 +11,33 @@ export default function Navs() {
   };
 
   return (
-    
-<></>
+    <div className="desktop-navs-wrapper">
+
+      <div className="desktop-navs-top">
+
+        <div
+          className={`desktop-hamburger ${open ? "active" : ""}`}
+          onClick={toggleMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+      </div>
+
+
+      <nav className={`desktop-navs-menu ${open ? "show" : ""}`}>
+
+        <Link href="/">Home</Link>
+        <Link href="/search">Travel</Link>
+        <Link href="/about">Pages</Link>
+        <Link href="/shop">Shop</Link>
+        <Link href="/blog">Blog</Link>
+        <Link href="/contact-us">Contact Us</Link>
+
+      </nav>
+
+    </div>
   );
 }
