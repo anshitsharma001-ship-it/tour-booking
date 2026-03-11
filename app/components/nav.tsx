439179394cfc72d7591a6cb91d710ae13ad99f5c
anshitsharma001-ship-it <anshitsharma001@gmail.com>
2026-03-11 19:57:22 +0530

    Update nav.tsx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Link from "next/link";
 
-export default function Navs() {
+export default function Nav() {
   const [open, setOpen] = useState(false);
 
   return (
