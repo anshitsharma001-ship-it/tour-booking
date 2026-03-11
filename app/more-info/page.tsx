0eebc526ae2844c04c997f2ae3caf702c8ffd043
anshitsharma001-ship-it <anshitsharma001@gmail.com>
2026-03-11 20:07:00 +0530

    more-info/page.tsx

---
+++ app/more-info/page.tsx
@@ -0,0 +1,8 @@
+export default function MoreInfo() {
+  return (
+    <div style={{ padding: "50px" }}>
+      <h1>More Info Page</h1>
+      <h2>Sorry, No information available yet</h2>
+    </div>
+  );
+}
